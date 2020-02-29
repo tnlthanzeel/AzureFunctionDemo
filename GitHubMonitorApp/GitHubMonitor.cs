822c4d23d0d7c61087945496e9e2204295d60cbf
Thanzeel <tnlthanzeel@gmail.com>
2020-02-29 13:51:33 +0530

    *deserialzie object to github object type

--- GitHubMonitorApp/GitHubMonitor.cs
+++ GitHubMonitorApp/GitHubMonitor.cs
@@ -12,24 +12,184 @@ namespace GitHubMonitorApp
 {
     public static class GitHubMonitor
     {
+        //name of the trigger that will be used by azure
         [FunctionName("GitHubMonitor")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            //           1. autherization level       2. http verbs  3. custome route            
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("Our github monitor processed an action");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var data = JsonConvert.DeserializeObject<Rootobject>(requestBody);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            log.LogInformation(JsonConvert.SerializeObject(data));
 
-            return new OkObjectResult(responseMessage);
+            return new OkResult();
         }
+
+
+
+        public class Rootobject
+        {
+            public string zen { get; set; }
+            public int hook_id { get; set; }
+            public Hook hook { get; set; }
+            public Repository repository { get; set; }
+            public Sender sender { get; set; }
+        }
+
+        public class Hook
+        {
+            public string type { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public bool active { get; set; }
+            public string[] events { get; set; }
+            public Config config { get; set; }
+            public DateTime updated_at { get; set; }
+            public DateTime created_at { get; set; }
+            public string url { get; set; }
+            public string test_url { get; set; }
+            public string ping_url { get; set; }
+            public Last_Response last_response { get; set; }
+        }
+
+        public class Config
+        {
+            public string content_type { get; set; }
+            public string insecure_ssl { get; set; }
+            public string url { get; set; }
+        }
+
+        public class Last_Response
+        {
+            public object code { get; set; }
+            public string status { get; set; }
+            public object message { get; set; }
+        }
+
+        public class Repository
+        {
+            public int id { get; set; }
+            public string node_id { get; set; }
+            public string name { get; set; }
+            public string full_name { get; set; }
+            public bool _private { get; set; }
+            public Owner owner { get; set; }
+            public string html_url { get; set; }
+            public object description { get; set; }
+            public bool fork { get; set; }
+            public string url { get; set; }
+            public string forks_url { get; set; }
+            public string keys_url { get; set; }
+            public string collaborators_url { get; set; }
+            public string teams_url { get; set; }
+            public string hooks_url { get; set; }
+            public string issue_events_url { get; set; }
+            public string events_url { get; set; }
+            public string assignees_url { get; set; }
+            public string branches_url { get; set; }
+            public string tags_url { get; set; }
+            public string blobs_url { get; set; }
+            public string git_tags_url { get; set; }
+            public string git_refs_url { get; set; }
+            public string trees_url { get; set; }
+            public string statuses_url { get; set; }
+            public string languages_url { get; set; }
+            public string stargazers_url { get; set; }
+            public string contributors_url { get; set; }
+            public string subscribers_url { get; set; }
+            public string subscription_url { get; set; }
+            public string commits_url { get; set; }
+            public string git_commits_url { get; set; }
+            public string comments_url { get; set; }
+            public string issue_comment_url { get; set; }
+            public string contents_url { get; set; }
+            public string compare_url { get; set; }
+            public string merges_url { get; set; }
+            public string archive_url { get; set; }
+            public string downloads_url { get; set; }
+            public string issues_url { get; set; }
+            public string pulls_url { get; set; }
+            public string milestones_url { get; set; }
+            public string notifications_url { get; set; }
+            public string labels_url { get; set; }
+            public string releases_url { get; set; }
+            public string deployments_url { get; set; }
+            public DateTime created_at { get; set; }
+            public DateTime updated_at { get; set; }
+            public DateTime pushed_at { get; set; }
+            public string git_url { get; set; }
+            public string ssh_url { get; set; }
+            public string clone_url { get; set; }
+            public string svn_url { get; set; }
+            public object homepage { get; set; }
+            public int size { get; set; }
+            public int stargazers_count { get; set; }
+            public int watchers_count { get; set; }
+            public string language { get; set; }
+            public bool has_issues { get; set; }
+            public bool has_projects { get; set; }
+            public bool has_downloads { get; set; }
+            public bool has_wiki { get; set; }
+            public bool has_pages { get; set; }
+            public int forks_count { get; set; }
+            public object mirror_url { get; set; }
+            public bool archived { get; set; }
+            public bool disabled { get; set; }
+            public int open_issues_count { get; set; }
+            public object license { get; set; }
+            public int forks { get; set; }
+            public int open_issues { get; set; }
+            public int watchers { get; set; }
+            public string default_branch { get; set; }
+        }
+
+        public class Owner
+        {
+            public string login { get; set; }
+            public int id { get; set; }
+            public string node_id { get; set; }
+            public string avatar_url { get; set; }
+            public string gravatar_id { get; set; }
+            public string url { get; set; }
+            public string html_url { get; set; }
+            public string followers_url { get; set; }
+            public string following_url { get; set; }
+            public string gists_url { get; set; }
+            public string starred_url { get; set; }
+            public string subscriptions_url { get; set; }
+            public string organizations_url { get; set; }
+            public string repos_url { get; set; }
+            public string events_url { get; set; }
+            public string received_events_url { get; set; }
+            public string type { get; set; }
+            public bool site_admin { get; set; }
+        }
+
+        public class Sender
+        {
+            public string login { get; set; }
+            public int id { get; set; }
+            public string node_id { get; set; }
+            public string avatar_url { get; set; }
+            public string gravatar_id { get; set; }
+            public string url { get; set; }
+            public string html_url { get; set; }
+            public string followers_url { get; set; }
+            public string following_url { get; set; }
+            public string gists_url { get; set; }
+            public string starred_url { get; set; }
+            public string subscriptions_url { get; set; }
+            public string organizations_url { get; set; }
+            public string repos_url { get; set; }
+            public string events_url { get; set; }
+            public string received_events_url { get; set; }
+            public string type { get; set; }
+            public bool site_admin { get; set; }
+        }
+
     }
 }
